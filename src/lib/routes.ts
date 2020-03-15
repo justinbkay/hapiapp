1e0e6b092d4b47dd337f8b3c9735c3f49d216365
Justin <justinbkay@gmail.com>
2020-03-15 12:03:41 -0600

    add 404 page and catch all route

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -30,5 +30,19 @@ export default [
         message: 'alsdf alsdf alsdkf asdf a'
       })
     }
+  },
+  {
+    method: [ 'GET', 'POST' ],
+    path: '/{any*}',
+    handler: (request: Request, h: ResponseToolkit) => {
+      const accept = request.headers['content-type']
+
+      if (accept && accept.match(/json/)) {
+        return Boom.notFound('This resource isn\'t available')
+      }
+
+      // @ts-ignore
+      return h.view('404').code(404)
+    }
   }
 ]
