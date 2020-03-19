3e3f714418099408003e096f92fba9a8c692f035
Justin <justin.kay@inxpress.com>
2020-03-19 14:37:48 -0600

    fix a couple of typescript return values

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,5 +1,5 @@
 import Boom from "@hapi/boom";
-import { ResponseToolkit, Request } from "@hapi/hapi";
+import { ResponseToolkit, Request, ResponseObject } from "@hapi/hapi";
 import got from "got"
 import { CatFact } from "./catfact"
 
@@ -29,8 +29,7 @@ export default [
   {
     method: 'GET',
     path: '/index',
-    handler: (request: Request, h: ResponseToolkit): string => { // eslint-disable-line @typescript-eslint/no-unused-vars
-      // @ts-ignore
+    handler: (request: Request, h: ResponseToolkit): ResponseObject => { // eslint-disable-line @typescript-eslint/no-unused-vars
       return h.view('index', {
         title: 'aosdmfasdf',
         message: 'alsdf alsdf alsdkf asdf a'
@@ -53,14 +52,13 @@ export default [
   {
     method: [ 'GET', 'POST' ],
     path: '/{any*}',
-    handler: (request: Request, h: ResponseToolkit): string | Boom.Boom => {
+    handler: (request: Request, h: ResponseToolkit): ResponseObject | Boom.Boom => {
       const accept = request.headers['content-type']
 
       if (accept && accept.match(/json/)) {
         return Boom.notFound('This resource isn\'t available')
       }
 
-      // @ts-ignore
       return h.view('404').code(404)
     }
   },
