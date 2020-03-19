bd96a21496f250ce7dfbce4a31e9d0f4ebe9806c
Justin <justin.kay@inxpress.com>
2020-03-19 13:30:42 -0600

    adding a few more tests

--- test/home.test.js
+++ test/home.test.js
@@ -24,6 +24,14 @@ describe('GET /', () => {
         expect(res.statusCode).to.equal(200);
     });
 
+    it('/ contains Hello World test', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/'
+        })
+        expect(res.result).to.include('Hello World')
+    })
+
     it('user/name responds with 200', async () => {
         const res = await server.inject({
             method: 'get',
@@ -32,6 +40,14 @@ describe('GET /', () => {
         expect(res.statusCode).to.equal(200);
     });
 
+    it('user/name contains Justin test', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/user/Justin'
+        })
+        expect(res.result).to.include('Justin')
+    })
+
     it('about responds with 200', async () => {
         const res = await server.inject({
             method: 'get',
@@ -39,6 +55,14 @@ describe('GET /', () => {
         });
         expect(res.statusCode).to.equal(200);
     });
+
+    it('/about contains about test', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: '/about'
+        })
+        expect(res.result).to.include('About')
+    })
 
     it('index responds with 200', async () => {
         const res = await server.inject({
