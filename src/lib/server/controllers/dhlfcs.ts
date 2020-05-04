7def61c48118fe948f940eb4ca17f83ede553316
Justin <justin.kay@inxpress.com>
2020-05-04 15:35:07 -0600

    ignore the date info

--- src/lib/server/controllers/dhlfcs.ts
+++ src/lib/server/controllers/dhlfcs.ts
@@ -1,16 +1,16 @@
 // import got from "got";
 import axios from "axios";
-import { DateTime } from "luxon";
+// import { DateTime } from "luxon";
 
-const dhlURL = "";
+const dhlURL = "http://www.dhl-usa.com/en/express/shipping/shipping_advice/express_fuel_surcharge.html";
 
 export async function handler(): Promise<number> {
-  const today = DateTime.local().toISODate();
+  // const today = DateTime.local().toISODate();
   let rate = 0;
   await axios
     .get(dhlURL)
     .then((response) => {
-      console.log(today);
+      // console.log(today);
       const matches: RegExpMatchArray | null = String(response.data).match(/>([\d.]+)%<.td>/);
       if (matches !== null) {
         rate = parseFloat(matches[1]);
